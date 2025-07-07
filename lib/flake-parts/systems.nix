d4d92517b4ef96fa6fbfcab1b4e69c1a184ba2ba
Najib Ibrahim <mnajib@gmail.com>
2025-07-07 10:54:29 +0800

    Removed nix/nixos/common reference from systems.nix

--- lib/flake-parts/systems.nix
+++ lib/flake-parts/systems.nix
@@ -1,5 +1,5 @@
 # my-nix-utils/lib/flake-parts/systems.nix
-{ inputs, lib, helpers, ... }: # Add 'helpers' here
+{ inputs, lib, helpers, ... }:
 
 let
   # These paths are now relative to the 'nixConfigRoot' managed by 'helpers'
@@ -7,7 +7,6 @@ let
   profilesUsersPath = "profiles/users";
 
   # Discover all host directories (excluding 'common')
-  # Returns: { hostname = "/absolute/path/to/nix/profiles/hosts/hostname"; ... }
   hostDirs = lib.filterAttrs (n: v: n != "common") (helpers.scanHostDirs profilesHostsPath);
 
   # Discover all user configurations
@@ -15,7 +14,7 @@ let
 
 in {
   perSystem = { system, pkgs, ... }: {
-    nixosConfigurations = lib.mapAttrs' (hostName: hostDirPath: # hostDirPath is now the absolute path to the host's directory
+    nixosConfigurations = lib.mapAttrs' (hostName: hostDirPath:
       let
         # Construct full paths to config files within the host's directory
         hostDefaultNix = "${hostDirPath}/default.nix";
@@ -53,8 +52,7 @@ in {
           modules = [
             # Global common host configuration (absolute path)
             "${helpers.nixConfigRoot}/${profilesHostsPath}/common/default.nix"
-            # ADD THIS LINE for `nix/nixos/common/default.nix`
-            "${helpers.nixConfigRoot}/nixos/common/default.nix"
+            # REMOVED: "${helpers.nixConfigRoot}/nixos/common/default.nix"
           ] ++ hostSpecificModules ++ [ # Concatenate the dynamically found host modules
             # Integrate Home-Manager for this host
             inputs.home-manager.nixosModules.home-manager {
@@ -69,6 +67,6 @@ in {
           specialArgs = { inherit inputs; };
         }
       )
-    ) hostDirs; # Use hostDirs to iterate through found host directories
+    ) hostDirs;
   };
 }
